c3e106dc19f0153d5569dc6e8b6a7e5902c95826
RubenMunoz <ruben.munoz@gracia.lasalle.cat>
2024-12-11 20:32:39 +0100

    He añadido las clases que representan al evento ademas del metodo string para enseñar el estado de una tarea

--- src/MunozRubenpractica2ej2.java
+++ src/MunozRubenpractica2ej2.java
@@ -1,6 +1,8 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
 // Enumeración para la prioridad de los eventos
 enum Priority {
     HIGH, MEDIUM, LOW;
@@ -19,5 +21,28 @@ class EventTask {
     // Uso de metodo para marcar como completada
     public void markAsCompleted() {
         this.isCompleted = true;
+    }
+    public void markAsIncomplete() {
+        this.isCompleted = false;
+    }
+
+    // Uso otro metodo para enseñar el estado de la tarea
+
+    public String toString() {
+        return "Tarea: " + text + " - " + (isCompleted ? "Completada" : "No completada");
+    }
+}
+// Clase que representa un evento
+class Event {
+    private String title;
+    private LocalDate date;
+    private Priority priority;
+    private ArrayList<EventTask> tasks;
+
+    public Event(String title, LocalDate date, Priority priority) {
+        this.title = title;
+        this.date = date;
+        this.priority = priority;
+        this.tasks = new ArrayList<>();
     }
 }
